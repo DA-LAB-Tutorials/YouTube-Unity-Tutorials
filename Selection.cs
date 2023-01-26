32ab369395e2e3139eb1992a1f2131f817d61de1
DA-LAB-Tutorials <122265695+DA-LAB-Tutorials@users.noreply.github.com>
2023-01-27 08:47:17 +1100

    Add files via upload

---
+++ Selection.cs
@@ -0,0 +1,93 @@
+//MIT License
+//Copyright (c) 2023 DA LAB (https://www.youtube.com/@DA-LAB)
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selection : MonoBehaviour
+{
+    public Material highlightMaterial;
+    public Material selectionMaterial;
+
+    private Material originalMaterial;
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    void Update()
+    {
+        if (highlight != null)
+        {
+            highlight.GetComponent<MeshRenderer>().material = originalMaterial; 
+            highlight = null; 
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+        {
+            highlight = raycastHit.transform; 
+            if (highlight.CompareTag("Selectable") && highlight != selection) 
+            {
+                if (highlight.GetComponent<MeshRenderer>().material != highlightMaterial)
+                {
+                    originalMaterial = highlight.GetComponent<MeshRenderer>().material; 
+                    highlight.GetComponent<MeshRenderer>().material = highlightMaterial; 
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.Mouse0) && !EventSystem.current.IsPointerOverGameObject()) //Make sure you have EventSystem in the hierarchy before using EventSystem
+        {
+            if (selection != null)
+            {
+                selection.GetComponent<MeshRenderer>().material = originalMaterial; 
+                selection = null; 
+            }
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) 
+            {
+                selection = raycastHit.transform; 
+                if (selection.CompareTag("Selectable")) 
+                {
+                    selection.GetComponent<MeshRenderer>().material = selectionMaterial; 
+                }
+                else
+                {
+                    selection = null;
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
