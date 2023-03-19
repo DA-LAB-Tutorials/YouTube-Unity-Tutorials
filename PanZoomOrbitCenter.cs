3f849e6c1a8b6ecc95751d4f67644fc39325915d
DA-LAB-Tutorials <122265695+DA-LAB-Tutorials@users.noreply.github.com>
2023-03-19 16:29:03 +1100

    Add files via upload

---
+++ PanZoomOrbitCenter.cs
@@ -0,0 +1,92 @@
+//MIT License
+//Copyright (c) 2023 DA LAB (https://www.youtube.com/@DA-LAB)
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PanZoomOrbitCenter : MonoBehaviour
+{
+    private float rotationSpeed = 500.0f;
+    private Vector3 mouseWorldPosStart;
+    private float zoomScale = 5.0f;
+    private float zoomMin = 0.5f;
+    private float zoomMax = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for Orbit, Pan, Zoom
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Mouse2) && !EventSystem.current.IsPointerOverGameObject()) //Check for orbit
+        {
+            CamOrbit();
+        }
+        if (Input.GetMouseButtonDown(2) && !Input.GetKey(KeyCode.LeftShift) && !EventSystem.current.IsPointerOverGameObject()) //Check for Pan
+        {
+            mouseWorldPosStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(2) && !Input.GetKey(KeyCode.LeftShift) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Pan();
+        }
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            Zoom(Input.GetAxis("Mouse ScrollWheel")); //Check for Zoom
+        }
+    }
+
+
+    private void CamOrbit()
+    {
+        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
+        {
+            float verticalInput = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.right, -verticalInput);
+            transform.Rotate(Vector3.up, horizontalInput, Space.World);
+        }
+    }
+
+    void Pan()
+    {
+        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
+        {
+            Vector3 mouseWorldPosDiff = mouseWorldPosStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += mouseWorldPosDiff;
+        }
+    }
+
+    void Zoom(float zoomDiff)
+    {
+        if (zoomDiff != 0)
+        {
+            mouseWorldPosStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - zoomDiff * zoomScale, zoomMin, zoomMax);
+            Vector3 mouseWorldPosDiff = mouseWorldPosStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += mouseWorldPosDiff;
+        }
+    }
+
+}
+
